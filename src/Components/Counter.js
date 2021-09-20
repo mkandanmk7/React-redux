1d27f7b7450f8adf5493d5da6350ceb79195fcc3
muthu <mkandanmk7@gmail.com>
2021-09-21 00:11:48 +0530

    redux getState dispatch()

--- src/Components/Counter.js
+++ src/Components/Counter.js
@@ -4,6 +4,11 @@ import { store } from "./store";
 export const Counter = () => {
   console.log(store);
   console.log(store.getState()); // current state count value; 0
+  console.log(store.dispatch({ type: "inc" })); // count inc = 1
+  console.log(store.dispatch({ type: "inc" })); // 2
+  console.log(store.dispatch({ type: "inc" })); // 3
+  console.log(store.getState()); //  curretn state value : 3
+
   return (
     <div>
       <h2>Counter Component</h2>
