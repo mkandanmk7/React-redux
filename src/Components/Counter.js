7d0fc93a943127535a87a5e992ea91c4471339a6
muthu <mkandanmk7@gmail.com>
2021-09-22 00:37:55 +0530

    set get

--- src/Components/Counter.js
+++ src/Components/Counter.js
@@ -7,6 +7,7 @@ export const Counter = () => {
   console.log(store.dispatch({ type: "inc" })); // count inc = 1
   console.log(store.dispatch({ type: "inc" })); // 2
   console.log(store.dispatch({ type: "inc" })); // 3
+  console.log(store.dispatch({ type: "inc" })); // 4
 
   console.log(store.getState()); //  curretn state value : 3
   console.log(store.dispatch({ type: "dec" })); // count dec = 2
